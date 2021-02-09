5b1441c813c3a0db2a8cb88b87a3ee1737242765
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-02-09 16:25:19 +0000

    Challenge 1 done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,11 +1,32 @@
-let js = 'amazing'
-if (js === 'amazing') alert('JavaScript is fun!');
+// let js = 'amazing'
+// if (js === 'amazing') alert('JavaScript is fun!');
 
-let language;
+// let language;
 
-console.log(typeof true);
-console.log(typeof null);
-console.log(typeof Symbol("foo"));
-console.log(typeof BigInt("0x1fffffffffffff"));
+// console.log(typeof true);
+// console.log(typeof null);
+// console.log(typeof Symbol("foo"));
+// console.log(typeof BigInt("0x1fffffffffffff"));
 
-language = "Polish";
+// language = "Polish";
+
+// challenge #1
+// BMI = mass / height ** 2
+
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const bmiMark = massMark / (heightMark ** 2);
+const bmiJohn = massJohn / (heightJohn ** 2);
+
+// logic replacing markHigherBMI variable
+if (bmiJohn > bmiMark) {
+	console.log(`John's BMI is bigger than Mark's by ${(bmiJohn - bmiMark).toFixed(1)}`);
+} else if (bmiMark === bmiJohn) {
+	console.log(`Both Mark and John have the same BMI of ${bmiJohn}`);
+} else {
+	console.log(`Mark's BMI is greater than John's by ${(bmiMark - bmiJohn).toFixed(1)}`);
+}
