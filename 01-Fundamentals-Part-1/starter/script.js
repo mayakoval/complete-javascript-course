7d34972fac752b230756a67d7b67ad5279991380
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-02-09 15:43:55 +0000

    Reassign the variable

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,7 +1,11 @@
 let js = 'amazing'
 if (js === 'amazing') alert('JavaScript is fun!');
 
+let language;
+
 console.log(typeof true);
 console.log(typeof null);
 console.log(typeof Symbol("foo"));
 console.log(typeof BigInt("0x1fffffffffffff"));
+
+language = "Polish";
