002bb7fa297cc1737e173573c1f10871dc3e83d7
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-02-09 15:41:08 +0000

    Transfer code to the right folder

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,7 @@
 let js = 'amazing'
-if (js === 'amazing') alert('JavaScript is fun!')
+if (js === 'amazing') alert('JavaScript is fun!');
+
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof Symbol("foo"));
+console.log(typeof BigInt("0x1fffffffffffff"));
