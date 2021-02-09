2580f4812aba6a9d0164d9772a3e3670ab6054d9
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-02-09 17:06:54 +0000

    Challenge 3 completed

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -13,20 +13,32 @@
 // challenge #1
 // BMI = mass / height ** 2
 
-const massMark = 78;
-const heightMark = 1.69;
+// const massMark = 78;
+// const heightMark = 1.69;
 
-const massJohn = 92;
-const heightJohn = 1.95;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-const bmiMark = massMark / (heightMark ** 2);
-const bmiJohn = massJohn / (heightJohn ** 2);
+// const bmiMark = massMark / (heightMark ** 2);
+// const bmiJohn = massJohn / (heightJohn ** 2);
 
-// logic replacing markHigherBMI variable
-if (bmiJohn > bmiMark) {
-	console.log(`John's BMI is bigger than Mark's by ${(bmiJohn - bmiMark).toFixed(1)}`);
-} else if (bmiMark === bmiJohn) {
-	console.log(`Both Mark and John have the same BMI of ${bmiJohn}`);
-} else {
-	console.log(`Mark's BMI is greater than John's by ${(bmiMark - bmiJohn).toFixed(1)}`);
+// // logic replacing markHigherBMI variable
+// if (bmiJohn > bmiMark) {
+// 	console.log(`John's BMI is bigger than Mark's by ${(bmiJohn - bmiMark).toFixed(1)}`);
+// } else if (bmiMark === bmiJohn) {
+// 	console.log(`Both Mark and John have the same BMI of ${bmiJohn}`);
+// } else {
+// 	console.log(`Mark's BMI is greater than John's by ${(bmiMark - bmiJohn).toFixed(1)}`);
+// }
+
+// challenge #3
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreKoalas === scoreDolphins) {
+	console.log(`It's a draw with both teams scoring ${scoreKoalas}!`);
+} else if (scoreKoalas > scoreDolphins) {
+	console.log(`Koalas win with ${scoreKoalas} vs ${scoreDolphins} for Dolphins`);
+} else if (scoreKoalas < scoreDolphins) {
+	console.log(`Dolphins win with ${scoreDolphins} vs ${scoreKoalas} for Koalas`);
 }
