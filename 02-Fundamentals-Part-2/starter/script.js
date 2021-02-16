a332638228abad43fea0c8268cdad68ddcaaebfd
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-02-16 11:24:37 +0000

    Start

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,23 @@
+// 'use strict';
 
+// let hasDriversLicence = false;
+// const passTest = true;
 
+// if (passTest) hasDriversLicence = true;
+
+// function sayHi(name) {
+// 	console.log(`Hi ${name}!`);
+// }
+
+// returning a value from one function and assigning it to a new variable
+
+// function fruitProcessor(apples, oranges) {
+// 	console.log(apples, oranges);
+// 	const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+// 	return juice;
+// }
+
+// call a function with chosen arguments
+
+// const newJuice = fruitProcessor(3, 2);
+// console.log(newJuice)
