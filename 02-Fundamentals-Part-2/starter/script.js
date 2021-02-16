d6a282411c1d846bdc38a15e56349ab60246a473
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-02-16 11:39:38 +0000

    Function types

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -21,3 +21,29 @@
 
 // const newJuice = fruitProcessor(3, 2);
 // console.log(newJuice)
+
+// function declaration
+// named function will work when called on the top of the code
+// preferrable to function expressions
+
+// const age = calcAge(1989);
+// console.log(age);
+
+// function calcAge(birthYear) {
+// 	d = new Date();
+// 	const currentYear = d.getFullYear();
+// 	return currentYear - birthYear;
+// }
+
+// console.log(calcAge(1990))
+
+// // unnamed function == function expression
+// // const age2 = calcAge2(1991); // won't work
+
+// const calcAge2 = function(birthYear) {
+// 	d = new Date();
+// 	const currentYear = d.getFullYear();
+// 	return currentYear - birthYear;
+// }
+
+// console.log(calcAge2(1990))
