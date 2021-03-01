563e55fda128c4fd74f1b4279499d3020a2ef5d7
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-03-01 11:41:52 +0000

    update for merging arrays

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -8,13 +8,50 @@
 
 // smart thermometer exercise
 
-const temperatures = [3, -2, -6, 1, "error", 9, 12, 16];
+// const temperatures = [3, -2, -6, 1, "error", 9, 12, 16];
 
-const calcAmplitude = function (temps) {
-	// method 2
+// const calcAmplitude = function (temps) {
+// 	// method 2
+// 	let temperatures = [];
+// 	for (let i = 0; i < temps.length; i++) {
+// 		const temperature = temps[i];
+// 		if (typeof temperature !== "number") continue;
+
+// 		temperatures.push(temperature);
+// 	}
+
+// 	const max = Math.max(...temperatures);
+// 	const min = Math.min(...temperatures);
+
+// 	return max - min;
+
+// 	// // method 1
+// 	// let highestTemp = temps[0];
+// 	// let lowestTemp = temps[0];
+
+// 	// for (let i = 0; i < temps.length; i++) {
+// 	// 	const temperature = temps[i];
+// 	// 	if (typeof temperature !== "number") continue;
+
+// 	// 	if (temperature > highestTemp) highestTemp = temperature;
+// 	// 	if (temperature < lowestTemp) lowestTemp = temperature;
+// 	// }
+// 	// console.log(highestTemp, lowestTemp);
+// 	// return highestTemp - lowestTemp;
+// };
+
+// console.log(calcAmplitude(temperatures));
+
+// merging arrays
+
+const temperatures1 = [3, -2, -6, 1, "error", 9, 12, 16];
+const temperatures2 = [23, 12, 5, "error", -1];
+
+const calcAmplitudeNew = function (arr1, arr2) {
+	const merged = arr1.concat(arr2);
 	let temperatures = [];
-	for (let i = 0; i < temps.length; i++) {
-		const temperature = temps[i];
+	for (let i = 0; i < merged.length; i++) {
+		const temperature = merged[i];
 		if (typeof temperature !== "number") continue;
 
 		temperatures.push(temperature);
@@ -24,20 +61,6 @@ const calcAmplitude = function (temps) {
 	const min = Math.min(...temperatures);
 
 	return max - min;
-
-	// // method 1
-	// let highestTemp = temps[0];
-	// let lowestTemp = temps[0];
-
-	// for (let i = 0; i < temps.length; i++) {
-	// 	const temperature = temps[i];
-	// 	if (typeof temperature !== "number") continue;
-
-	// 	if (temperature > highestTemp) highestTemp = temperature;
-	// 	if (temperature < lowestTemp) lowestTemp = temperature;
-	// }
-	// console.log(highestTemp, lowestTemp);
-	// return highestTemp - lowestTemp;
 };
 
-console.log(calcAmplitude(temperatures));
+console.log(calcAmplitudeNew(temperatures1, temperatures2));
