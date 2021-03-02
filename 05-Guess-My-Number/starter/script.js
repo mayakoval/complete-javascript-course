adc7f999ed3e7cca132e35ecebd77115bbcb26d7
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-03-02 15:16:47 +0000

    Basic logic added

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,11 +1,30 @@
 'use strict';
 
-// sandbox
+// // sandbox
 
-console.log(document.querySelector('.message'));
-document.querySelector('.message').textContent = 'Correct number!';
+// console.log(document.querySelector('.message'));
+// document.querySelector('.message').textContent = 'Correct number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 19;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 19;
 
-document.querySelector('.guess').value = 11;
+// document.querySelector('.guess').value = 11;
+
+// game logic
+
+// random number generator, adds 1 to include 20 as the last possibility
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = randomNumber;
+
+console.log(randomNumber);
+
+document.querySelector('.check').addEventListener('click', function () {
+	const guess = Number(document.querySelector('.guess').value);
+	console.log(guess, typeof guess);
+
+	if (!guess) {
+		document.querySelector('.message').textContent = 'No number chosen';
+	} else if (guess === randomNumber) {
+		document.querySelector('.message').textContent = 'Correct anwser!';
+	}
+});
