467419e6317ba7c1ad373dd279c058ef6c058a71
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-03-02 15:49:15 +0000

    Game initialized

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,14 +14,15 @@
 
 // random number generator, adds 1 to include 20 as the last possibility
 const randomNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = randomNumber;
+let score = 20;
+
+document.querySelector('.score').textContent = score;
 
 console.log(randomNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
 	const guess = Number(document.querySelector('.guess').value);
 	// console.log(guess, typeof guess);
-
 	if (!guess) {
 		document.querySelector('.message').textContent = 'No number chosen';
 	} else if (guess > 20 || guess < 1) {
@@ -29,9 +30,26 @@ document.querySelector('.check').addEventListener('click', function () {
 			'Number has to be between 1 to 20!';
 	} else if (guess === randomNumber) {
 		document.querySelector('.message').textContent = 'Correct anwser!';
+		document.querySelector('.number').textContent = randomNumber;
 	} else if (guess > randomNumber) {
-		document.querySelector('.message').textContent = 'Too high, try again';
+		// guard clause
+		if (score > 1) {
+			document.querySelector('.message').textContent = 'Too high, try again';
+			score--;
+			document.querySelector('.score').textContent = score;
+		} else {
+			document.querySelector('.message').textContent = 'Game over';
+			document.querySelector('.score').textContent = 0;
+		}
 	} else if (guess < randomNumber) {
-		document.querySelector('.message').textContent = 'Too low, try again';
+		// guard clause
+		if (score > 1) {
+			document.querySelector('.message').textContent = 'Too low, try again';
+			score--;
+			document.querySelector('.score').textContent = score;
+		} else {
+			document.querySelector('.message').textContent = 'Game over';
+			document.querySelector('.score').textContent = 0;
+		}
 	}
 });
