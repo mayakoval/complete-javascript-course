2033ffc7b8b1de792ca33c03ff91bdbb5ae0dc1f
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-03-02 15:00:29 +0000

    DOM testing

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// sandbox
+
+console.log(document.querySelector('.message'));
+document.querySelector('.message').textContent = 'Correct number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 19;
+
+document.querySelector('.guess').value = 11;
