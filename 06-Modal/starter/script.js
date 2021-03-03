3469998a39b969b0b8de28ea128a8dc624fa93f4
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-03-03 15:29:27 +0000

    Close and show modal functions created

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,10 +5,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsShowModal.length; i++)
-	btnsShowModal[i].addEventListener('click', function () {
-		console.log('Button clicked');
-		modal.classList.remove('hidden');
-	});
+const showModal = function () {
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+};
 
-const showModal = function () {};
+const closeModal = function () {
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++)
+	btnsShowModal[i].addEventListener('click', showModal);
+
+// closeModal doesn't require empty brackets
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
