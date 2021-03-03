c5ecd350ceea3c58b18608ddf3befab15c007757
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-03-03 15:20:03 +0000

    Modal shows

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsShowModal.length; i++)
+	btnsShowModal[i].addEventListener('click', function () {
+		console.log('Button clicked');
+		modal.classList.remove('hidden');
+	});
+
+const showModal = function () {};
