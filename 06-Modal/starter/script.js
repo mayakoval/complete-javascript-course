d5068e64d9e47b4a1a04197c7c51f52de3f05e51
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-03-03 15:43:24 +0000

    Event listener for keydown

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -22,3 +22,11 @@ for (let i = 0; i < btnsShowModal.length; i++)
 // adding brackets would trigger the function without clicking
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// e for event
+document.addEventListener('keydown', function (e) {
+	// close the modal if not hidden
+	if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+		closeModal();
+	}
+});
