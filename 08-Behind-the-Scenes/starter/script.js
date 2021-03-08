5225a30fb98fbb015aae2f879733b881fc734bb1
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2021-03-08 19:45:57 +0000

    Global variable created

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// function scope
+
+function calcAge(birthYear) {
+	const age = 2021 - birthYear;
+	// will show as firstName is available in global scope
+	console.log(firstName);
+	return age;
+}
+
+// global variable
+
+const firstName = 'John';
+calcAge(1985);
